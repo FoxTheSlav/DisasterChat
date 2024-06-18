88519f9bd29bae5c0c5eb25acf1bb042a9916cf1
TheSlavonicFox <the.slavonic.fox@gmail.com>
2024-06-18 13:31:23 +0300

    Add files via upload

--- Session/State.cs
+++ Session/State.cs
@@ -1,1 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace DisasterServer.Session
+{
+    public enum State : byte
+    {
+        LOBBY
+    }
+}
